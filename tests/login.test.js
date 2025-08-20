4e0a5b56af9e776fc0bfe78b9cd650acbd239d22
Leticia <le-dalcol@hotmail.com>
2025-08-20 19:28:41 -0300

    Adicionado o arquivo inicial com teste de login

---
+++ tests/login.test.js
@@ -0,0 +1,35 @@
+import http from 'k6/http'
+import { sleep, check } from 'k6'
+
+export const options = {
+  iterations: 10000,
+  thresholds:{ 
+    http_req_duration: ['p(90)<10', 'max<1'],
+     http_req_failed: ['rate<0.01']
+  }  
+};
+
+export default function () {
+    const url = 'http://localhost:3000/login';
+
+    const payload = JSON.stringify  ({
+    username: 'julio.lima',
+    senha: '123456',
+    });
+
+    const params = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+ };
+
+  const res = http.post(url, payload, params);
+  
+  check (res, {
+    'Validar que Status é 200': (r) => r.status === 200,
+    'Validar que o Token é string': (r) => typeof(r.json().token =='string')
+  })
+  console.log(resposta)
+
+  sleep (1);
+}
